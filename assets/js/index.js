6f261b2a8154338dc72c0325eea532ba788c63d9
“alex” <poryvvotryv@gmail.com>
2024-02-02 21:20:59 +0200

    Fix bug and add chek handler

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,12 +1,27 @@
-function showNum() {
-    let i = 0;
+const defenitionRange = prompt("Till what number we will look for prime numbers?", 100)
 
-    while(true){
-        i++;
-        if(i > 100) inner: break
-        console.log(i);
+function showPrimeNum(till) {
+    let check = Number(till);
+
+    if(Number.isNaN(check)) {
+        alert("Don`t see a number! Pls, come again)")
+        let againTryToRange = prompt("Till what number we will look for prime numbers?", 100)
+        showPrimeNum(againTryToRange)
+    };
+
+    if(check <= 2) {
+        alert("An insufficient number! Pls, come again)")
+        let againTryToRange = prompt("Till what number we will look for prime numbers?", 100)
+        showPrimeNum(againTryToRange)
+     };
+ 
+    nextPrime:for(let i = 2;i < check;i++){
+
+        for(let k = 2;k < i;k++){
+            if(i % k === 0) continue nextPrime;
+            }
+            console.log(`${i} is a prime number`)
+        }
     }
-    outer: return
-}
 
-showNum()
+showPrimeNum(defenitionRange)
